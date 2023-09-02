46f136e30ad52d6ecbebcbba9df92b40fd5ed11b
VladimirLoktin21 <142094719+VladimirLoktin21@users.noreply.github.com>
2023-09-02 18:08:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-Код в проекте создан для того что бы изучать иностранные языки и добавлять слова которых нет в том коде
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'Пока :( {bot.user}!')    
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')    
+
+bot.run("MTE0NDk2MDA4OTk1OTE5MDY2OA.G3lVyF.Ulj6D8bNTg9TJ-H4e3e2iCoulLllOblo0s98iA")
