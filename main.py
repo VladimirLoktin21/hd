117a594dcd381e2a85364261092d86b346b2497b
VladimirLoktin21 <142094719+VladimirLoktin21@users.noreply.github.com>
2024-02-10 18:42:01 +0600

    Ai bot

--- main.py
+++ main.py
@@ -1,85 +1,88 @@
 main.py
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-def result_calculate(size, lights, device):
-    home_coef = 100
-    light_coef = 0.04
-    devices_coef = 5   
-    return size * home_coef + lights * light_coef + device * devices_coef 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/<size>')
-def lights(size):
-    return render_template('lights.html', size=size)
-
-@app.route('/<size>/<lights>')
-def electronics(size, lights):
-    return render_template('electronics.html', size=size, lights=lights)
-
-@app.route('/<size>/<lights>/<device>')
-def end(size, lights, device):
-    result = result_calculate(int(size), int(lights), int(device))
-    return render_template('end.html', result=result)
-
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    name = request.form['name']
-    email = request.form['email']
-    address = request.form['address']  
-    date = request.form['date']
+import discord
+from discord.ext import commands
+from os import remove
+from time import sleep
+from cl_model import get_class
 
 
-    with open('form.txt', 'a') as f:
-        f.write(f"Name: {name}\n")
-        f.write(f"Email: {email}\n")
-        f.write(f"Address: {address}\n")
-        f.write(f"Date: {date}\n\n")
+intents = discord.Intents.default()
+intents.message_content = True
 
-    return render_template('form_result.html', name=name, email=email, address=address, date=date)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-form_result.html
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../../static/css/style.css">
-    <title>Form Submission Result</title>
-</head>
-<body>
-    <header class="header">
-        <div class="header__text">
-            <h1>Form Submission Result</h1>
-        </div>
-    </header>
-    <main>
-        <div class="container">
-            <h2>Form Data</h2>
-            <ul>
-                <li><strong>Name:</strong> {{ name }}</li>
-                <li><strong>Email:</strong> {{ email }}</li>
-                <li><strong>Address:</strong> {{ address }}</li>
-                <li><strong>Date of Service:</strong> {{ date }}</li>
-            </ul>
-            <a href="/form">Return to Form</a>
-        </div>
-    </main>
-    <footer>
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
 
-    </footer>
-</body>
-</html>
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def photo(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            if file_name.endswith('.jpg') or file_name.endswith('.jpeg') or file_name.endswith('.png'):
+                await attachment.save(f'./images/{file_name}')
+                msg = await ctx.send('Пытаюсь разобрать что на фото...')
+                class_name, confidence = get_class(model_path='model/keras_model.h5', labels_path='model/labels.txt', image_path=f'/images/{file_name}')
+                await msg.delete()
+                await ctx.send(f'С вероятностью {confidence}% на фото {class_name.lower()}')
+                remove(f'./images/{file_name}')
+            else:
+                await ctx.send('Неправильный формат файлов! (мне нужны картинки)')
+                return
+    else:
+        await ctx.send('Кажется, ты забыл прикрепить фотографии ;c')
+bot.run("MTIwMzMwMjY0Nzk0NDE4MzgyOA.GLGwt7.85RT4jfxCpaOtLMR6W1y3PjP7xXtE4y8coOQls")
+
+cl_model.py
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
+
+def get_class(model_path, labels_path, image_path):
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
+
+    # Load the model
+    model = load_model("keras_Model.h5", compile=False)
+
+    # Load the labels
+    class_names = open("labels.txt", "r", encoding='utf-8').readlines()
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    image = Image.open("<IMAGE_PATH>").convert("RGB")
+
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    return class_name[2:len(class_name)], round(confidence_score)
